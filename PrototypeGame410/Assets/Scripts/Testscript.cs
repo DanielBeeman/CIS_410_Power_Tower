033afe420e920d227cfa8d6d1355388d9cc83138
ChandlerPotter <cpotter3@uoregon.edu>
2019-05-03 11:59:15 -0700

    PoC build, multiple enemies

---
+++ PrototypeGame410/Assets/Scripts/Testscript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testscript : MonoBehaviour
+{
+    public Rigidbody bulletPrefab;
+    private float time = 0.0f;
+    public float interpolationPeriod = 10.0f;
+
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        time += Time.deltaTime;
+        GameObject theEnemy = GameObject.Find("Enemy");
+        EnemyHealth enemy = theEnemy.GetComponent<EnemyHealth>();
+
+        if (enemy.alive == true)
+        {
+            InvokeRepeating("Shoot", 0.3f, 0.3f);
+        }
+    }
+
+    public void Shoot()
+    {
+        GameObject thePlayer = GameObject.Find("Enemy");
+        EnemyHealth enemyHealth = thePlayer.GetComponent<EnemyHealth>();
+
+        GameObject theTurret = GameObject.Find("Tank_Location");
+        Tank_Location_Script turret = theTurret.GetComponent<Tank_Location_Script>();
+
+
+        GameObject theTurret2 = GameObject.Find("Tank_Location_2");
+        Tank_Location_Script turret2 = theTurret2.GetComponent<Tank_Location_Script>();
+
+
+        GameObject theTurret3 = GameObject.Find("TurretTower");
+        Tank_Location_Script turret3 = theTurret3.GetComponent<Tank_Location_Script>();
+
+
+        if (enemyHealth.alive == true && turret.isPlaced == true)
+        {
+            Instantiate(bulletPrefab, new Vector3(turret.transform.position.x, turret.transform.position.y + 3, turret.transform.position.z), gameObject.transform.rotation);
+        }
+
+        if (enemyHealth.alive == true && turret2.isPlaced == true)
+        {
+            Instantiate(bulletPrefab, new Vector3(turret2.transform.position.x, turret2.transform.position.y + 3, turret2.transform.position.z), gameObject.transform.rotation);
+        }
+
+        if (enemyHealth.alive == true && turret3.isPlaced == true)
+        {
+            Instantiate(bulletPrefab, new Vector3(turret3.transform.position.x, turret3.transform.position.y + 3, turret3.transform.position.z), gameObject.transform.rotation);
+        }
+    }
+}
---
+++ PrototypeGame410/Assets/Scripts/movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 movement = new Vector3(5.0f, 0.0f, 5.0f);
+        transform.Translate(Vector3.forward * speed);
+
+        //rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Endpoint"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
